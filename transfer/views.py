6430dd39d4e46518c137d8a452badda89c59dc3e
unknown <jolaosoyusuf@gmail.com>
2019-01-04 18:03:50 +0100

    listing banks on transfer page

--- transfer/views.py
+++ transfer/views.py
@@ -1,8 +1,17 @@
-# import requests
+import requests
+import os
 from django.shortcuts import render
 # from django.http import HttpResponse
 
 # Create your views here.
 
 def transfer(request):
-    return render(request, 'transfer/index.html', context=None)
+    RAVE_PUBLIC_KEY = os.environ['RAVE_PUBLIC_KEY']
+    url = 'https://ravesandboxapi.flutterwave.com/v2/banks/NG?public_key=' + RAVE_PUBLIC_KEY
+    # print (url)
+    bank_lists = requests.get(url).json()
+    banks = bank_lists["data"]["Banks"]
+    context = {
+        "banks": banks
+    }
+    return render(request, 'transfer/index.html', context)
