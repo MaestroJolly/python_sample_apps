97a329ebeebebd326f338658de0b361b2fd2d80f
unknown <jolaosoyusuf@gmail.com>
2019-01-16 18:52:22 +0100

    webhook response logging to database

--- webhook/models.py
+++ webhook/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Log(models.Model):
+    customer_name = models.CharField(max_length=30)
+    customer_email = models.CharField(max_length=30)
+    status = models.CharField(max_length=10)
+    transaction_type = models.CharField(max_length=20)
+    narration = models.CharField(max_length=50)
+    account = models.CharField(max_length=10)
+    amount = models.IntegerField()
+    currency = models.CharField(max_length=3)
+    transaction_ref = models.CharField(max_length=35)
+    flw_ref = models.CharField(max_length=35)
+    created_at = models.CharField(max_length=26)
+
+    def __str__(self):
+        return self.transaction_type
--- webhook/views.py
+++ webhook/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from .models import Log
 
 # Create your views here.
 @require_http_methods(["GET", "POST"])
@@ -12,7 +13,12 @@ def my_webhook_view(request):
   request_json = request.body
 
   # Do something with request_json
-  print (request_json)
+  data = json.loads(request_json)
+  # print (data)
+  if data["event.type"] == "Transfer" or data["transfer"]["reference"] == None:
+    resp = Log.objects.create(customer_name=data["transfer"]["fullname"], status=data["transfer"]["status"], transaction_type=data["event.type"], amount=data["transfer"]["amount"], currency=data["transfer"]["currency"], created_at=data["transfer"]["date_created"])
+  else:
+    resp = Log.objects.create(customer_name=data["customer"]["fullName"], customer_email=data["customer"]["email"], status=data["status"], transaction_type=data["event.type"], amount=data["amount"], currency=data["currency"], transaction_ref=data["txRef"], flw_ref=data["flwRef"], created_at=data["customer"]["createdAt"])
   return HttpResponse()
 
 # def my_webhook_view(request):
