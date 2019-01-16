dfeacde21a297a40178cff57aa47a1a88d407405
unknown <jolaosoyusuf@gmail.com>
2019-01-16 22:44:15 +0100

    webhook logging to database

--- webhook/views.py
+++ webhook/views.py
@@ -11,14 +11,13 @@ from .models import Log
 def my_webhook_view(request):
   # Retrieve the request's body
   request_json = request.body
-
   # Do something with request_json
   data = json.loads(request_json)
-  # print (data)
-  if data["event.type"] == "Transfer" or data["transfer"]["reference"] == None:
+  print (data)
+  if data["event.type"] == "Transfer":
     resp = Log.objects.create(customer_name=data["transfer"]["fullname"], status=data["transfer"]["status"], transaction_type=data["event.type"], amount=data["transfer"]["amount"], currency=data["transfer"]["currency"], created_at=data["transfer"]["date_created"])
   else:
-    resp = Log.objects.create(customer_name=data["customer"]["fullName"], customer_email=data["customer"]["email"], status=data["status"], transaction_type=data["event.type"], amount=data["amount"], currency=data["currency"], transaction_ref=data["txRef"], flw_ref=data["flwRef"], created_at=data["customer"]["createdAt"])
+    resp = Log.objects.create(customer_name=data["customer"]["fullName"], customer_email=data["customer"]["email"],status=data["status"], transaction_type=data["event.type"], amount=data["amount"], currency=data["currency"], transaction_ref=data["txRef"], flw_ref=data["flwRef"], created_at=data["customer"]["createdAt"])
   return HttpResponse()
 
 # def my_webhook_view(request):
